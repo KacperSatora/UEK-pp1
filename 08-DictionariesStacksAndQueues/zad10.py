5062ee991770db388bc7f21b54cb2b4813caba35
Kacper Satora <kacpersatora02@gmail.com>
2022-12-02 17:59:20 +0100

    stuff dump

---
+++ 08-DictionariesStacksAndQueues/zad10.py
@@ -0,0 +1,13 @@
+arr = [
+    {"Poland": "38m"},
+    {"Germany": "60m"},
+    {"France": "50m"},
+    {"Italy": "45m"},
+    {"Denmark": "5m"}
+]
+
+count = 0
+length = len(arr)
+while count < length:
+    print(arr[count])
+    count += 1
---
+++ 08-DictionariesStacksAndQueues/zad9.py
@@ -0,0 +1,11 @@
+phone = {
+    "brand": "Nokia",
+    "IMEI": 1234567890,
+    "ScreenSize": 6.5,
+    "Battery": 4500,
+    "YearOfProduction": 2018,
+    "isWorking": True
+}
+
+for i,j in phone.items():
+    print(i, j)
