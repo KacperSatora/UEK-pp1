45fb4365d720cc83ca951207beaa954c9fdc1d9b
Kacper Satora <kacpersatora02@gmail.com>
2022-12-05 10:18:20 +0100

    07/22 csv redone

--- 07-FileHandling/zad22.py
+++ 07-FileHandling/zad22.py
@@ -1,11 +1,14 @@
 import csv
 csvList = []
 
-with open("t22.csv", "r") as csvfile:
-    for line in csvfile:
-        line = line.strip()
-        line = line.split(',')
-        csvList.append(line)
+with open("t22.csv", "r") as csvFile:
+    csvData = csv.reader(csvFile)
+    for line in csvData:
+        print('{:<15} {:<15} {:<20}'.format(line[0], line[1], line[4]))
+#     for line in csvFile:
+#         line = line.strip()
+#         line = line.split(',')
+#         csvList.append(line)
 
-for i in range(1,len(csvList)):
-    print('{:<15} {:<15} {:<20}'.format(csvList[i][0], csvList[i][1], csvList[i][4]))
+# for i in range(1,len(csvList)):
+#     print('{:<15} {:<15} {:<20}'.format(csvList[i][0], csvList[i][1], csvList[i][4]))
