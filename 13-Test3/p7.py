98417f474fd4ffd6db7be764a8df27c5e29874c1
Kacper Satora <68196071+KacperSatora@users.noreply.github.com>
2023-01-09 15:15:35 +0100

    Update p7.py
    Added @staticmethod

--- 13-Test3/p7.py
+++ 13-Test3/p7.py
@@ -3,18 +3,18 @@ import re
 
 class C:
 
-    
+    @staticmethod
     def m1(n):
         oddNumbers = re.findall(r"[24680]", str(n))
         return "".join(oddNumbers)
-
+    @staticmethod
     def m2(n):
         digits = list(str(n))
         for i in range(1, len(digits)):
             if digits[i] < digits[i - 1]:
                 return False
         return True
-
+    @staticmethod
     def m3(n):
         modelSet = set(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
         digits = set(list((str(n))))
